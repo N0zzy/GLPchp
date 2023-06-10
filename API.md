e0760941d94425cb82d255bd5fda0dee0c5b97fa
N0zzy <ffnn83@yandex.ru>
2023-06-10 23:34:01 +0500

    0.2.0

---
+++ API.md
@@ -0,0 +1,59 @@
+# class `CallableFunc`
+
+## method `On`
+
+
+public function `On` (string  *`$event`*, PhpValue *`$obj`*, PhpCallable *`$action`*, array *`$args`* = null) : ***void***
+
+| Type        | Arg Name | Default | Description             |
+|:------------|:---------|:--------|:------------------------|
+| string      | event    |         | name action for connect |
+| PhpValue    | obj      |         | object in godot         |
+| PhpCallable | action   |         | callable function       |
+| array       | args     | null    | arguments for function  |
+
+> **return**: *void*
+
+## method `OnListener`
+
+> adding a function or method (callable)
+
+public Callable `OnListener`(string *`$event`*, PhpValue *`$current`*, PhpArray *`$listener`*, array *`$args`* = null): ***void***
+
+| Type        | Arg Name | Default | Description                |
+|:------------|:---------|:--------|:---------------------------|
+| string      | event    |         | name action for connect    |
+| PhpValue    | current  |         | object in godot            |
+| PhpCallable | listener |         | array(2)\[object, method\] |
+| array       | args     | null    | arguments for function     |
+
+> **return**:  *void*
+
+> ### method `OnListenerAll` (all modifier)
+
+> adding a function or method (callable)
+
+public Callable `OnListenerAll`(string *`$event`*, PhpValue *`$current`*, PhpArray *`$listener`*, array *`$args`* = null): void
+
+| Type        | Arg Name | Default | Description                |
+|:------------|:---------|:--------|:---------------------------|
+| string      | event    |         | name action for connect    |
+| PhpValue    | current  |         | object in godot            |
+| PhpCallable | listener |         | array(2)\[object, method\] |
+| array       | args     | null    | arguments for function     |
+
+> **return**:  *void
+
+> ### method `Get`
+
+> adding a function (callable)
+
+public static `Get`(PhpCallable *`$action`*, array *`$args`* = null): ***\Godot\Callable***
+
+| Type        | Arg Name | Default | Description            |
+|:------------|:---------|:--------|:-----------------------|
+| PhpCallable | $action  |         | callable function      |
+| array       | $args    | null    | arguments for function |
+
+> **return**:  *\Godot\Callable*
+
--- CallableFunc.cs
+++ CallableFunc.cs
@@ -1,12 +1,96 @@
 using System;
+using System.Reflection;
 using Godot;
+using Pchp.Core;
+using static System.Reflection.BindingFlags;
 
 namespace GLPchp;
 
 public class CallableFunc
 {
-    public Callable From(Action action)
+    private static readonly BindingFlags _bindingFlagsBasic =
+        Instance | Static | NonPublic;
+    private static readonly BindingFlags _bindingFlagsAll =
+        Instance | Static | Public | NonPublic;
+    
+    public void On(string @event, PhpValue current, PhpCallable func, PhpValue[] args = null)
     {
-        return Callable.From(action);
+        GodotObject gObj = (GodotObject)current.AsObject(); 
+        gObj.Connect(new StringName(@event), Callable.From(() =>
+        {
+            func.Invoke(null, args);
+        }));
+    }
+    
+    public void OnListener(string @event, PhpValue current, PhpArray listener, object[] args = null)
+    {
+        GodotObject gObj = (GodotObject)current.AsObject(); 
+        gObj.Connect(new StringName(@event), Callable.From(() =>
+        {
+            ExecuteListener(listener, _bindingFlagsBasic, args);
+        }));
+    }
+    
+    public void OnListenerAll(string @event, PhpValue current, PhpArray listener, object[] args = null)
+    {
+        GodotObject gObj = (GodotObject)current.AsObject(); 
+        gObj.Connect(new StringName(@event), Callable.From(() =>
+        {
+            ExecuteListener(listener, _bindingFlagsAll, args);
+        }));
+    }
+    
+    public static Callable Get(PhpCallable action, PhpValue[] args = null)
+    {
+        return Callable.From(() =>
+        {
+            action.Invoke(null, args);
+        });
+    }
+    
+    public static Callable GetBind(object current, string func, object[] args = null)
+    {
+        return Callable.From(() =>
+        {
+            var methodInfo = current.GetType().GetMethod(func, _bindingFlagsBasic);
+            if (methodInfo != null) methodInfo.Invoke(current, args);
+        });
+    }
+    
+    public static Callable GetBindAll(object current, string func, object[] args = null)
+    {
+        return Callable.From(() =>
+        {
+            var methodInfo = current.GetType().GetMethod(func, _bindingFlagsAll);
+            if (methodInfo != null) methodInfo.Invoke(current, args);
+        });
+    }
+    
+    public static Callable GetListener(PhpArray listener, object[] args = null)
+    {
+        return Callable.From(() =>
+        {
+            ExecuteListener(listener, _bindingFlagsBasic, args);
+        });
+    }
+    
+    public static Callable GetListenerAll(PhpArray listener, object[] args = null)
+    {
+        return Callable.From(() =>
+        {
+            ExecuteListener(listener, _bindingFlagsAll, args);
+        });
+    }
+
+    public void Off()
+    {
+        
+    }
+    
+    private static void ExecuteListener(PhpArray listener, BindingFlags flags, object[] args)
+    {
+        var listenerObject = listener[0].AsObject();
+        var listenerFuncName = listener[1].ToString();
+        listenerObject.GetType().GetMethod(listenerFuncName, flags)?.Invoke(listenerObject, args);
     }
 }
