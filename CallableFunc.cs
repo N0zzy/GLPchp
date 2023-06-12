26db204d4f37edb557e3ac9793a6a485af2b7d36
N0zzy <ffnn83@yandex.ru>
2023-06-12 19:40:07 +0500

    0.2.1

--- CallableFunc.cs
+++ CallableFunc.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Reflection;
 using Godot;
 using Pchp.Core;
@@ -13,6 +12,13 @@ public class CallableFunc
     private static readonly BindingFlags _bindingFlagsAll =
         Instance | Static | Public | NonPublic;
     
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="event"></param>
+    /// <param name="current"></param>
+    /// <param name="func"></param>
+    /// <param name="args"></param>
     public void On(string @event, PhpValue current, PhpCallable func, PhpValue[] args = null)
     {
         GodotObject gObj = (GodotObject)current.AsObject(); 
@@ -22,7 +28,14 @@ public class CallableFunc
         }));
     }
     
-    public void OnListener(string @event, PhpValue current, PhpArray listener, object[] args = null)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="event"></param>
+    /// <param name="current"></param>
+    /// <param name="listener"></param>
+    /// <param name="args"></param>
+    public void OnListener(string @event, PhpValue current, PhpArray listener, PhpValue[] args = null)
     {
         GodotObject gObj = (GodotObject)current.AsObject(); 
         gObj.Connect(new StringName(@event), Callable.From(() =>
@@ -31,7 +44,14 @@ public class CallableFunc
         }));
     }
     
-    public void OnListenerAll(string @event, PhpValue current, PhpArray listener, object[] args = null)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="event"></param>
+    /// <param name="current"></param>
+    /// <param name="listener"></param>
+    /// <param name="args"></param>
+    public void OnListenerAll(string @event, PhpValue current, PhpArray listener, PhpValue[] args = null)
     {
         GodotObject gObj = (GodotObject)current.AsObject(); 
         gObj.Connect(new StringName(@event), Callable.From(() =>
@@ -40,6 +60,12 @@ public class CallableFunc
         }));
     }
     
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="action"></param>
+    /// <param name="args"></param>
+    /// <returns></returns>
     public static Callable Get(PhpCallable action, PhpValue[] args = null)
     {
         return Callable.From(() =>
@@ -48,25 +74,43 @@ public class CallableFunc
         });
     }
     
-    public static Callable GetBind(object current, string func, object[] args = null)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="current"></param>
+    /// <param name="func"></param>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static Callable GetBind(object current, string func, PhpValue[] args = null)
     {
         return Callable.From(() =>
         {
-            var methodInfo = current.GetType().GetMethod(func, _bindingFlagsBasic);
-            if (methodInfo != null) methodInfo.Invoke(current, args);
+            __InvokeGet(current,_bindingFlagsBasic, func, args);
         });
     }
     
-    public static Callable GetBindAll(object current, string func, object[] args = null)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="current"></param>
+    /// <param name="func"></param>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static Callable GetBindAll(object current, string func, PhpValue[] args = null)
     {
         return Callable.From(() =>
         {
-            var methodInfo = current.GetType().GetMethod(func, _bindingFlagsAll);
-            if (methodInfo != null) methodInfo.Invoke(current, args);
+            __InvokeGet(current,_bindingFlagsAll, func, args);
         });
     }
     
-    public static Callable GetListener(PhpArray listener, object[] args = null)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="listener"></param>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static Callable GetListener(PhpArray listener, PhpValue[] args = null)
     {
         return Callable.From(() =>
         {
@@ -74,7 +118,13 @@ public class CallableFunc
         });
     }
     
-    public static Callable GetListenerAll(PhpArray listener, object[] args = null)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="listener"></param>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static Callable GetListenerAll(PhpArray listener, PhpValue[] args = null)
     {
         return Callable.From(() =>
         {
@@ -82,15 +132,47 @@ public class CallableFunc
         });
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     public void Off()
     {
         
     }
     
-    private static void ExecuteListener(PhpArray listener, BindingFlags flags, object[] args)
+    private static void ExecuteListener(PhpArray listener, BindingFlags flags, PhpValue[] args = null)
     {
         var listenerObject = listener[0].AsObject();
         var listenerFuncName = listener[1].ToString();
-        listenerObject.GetType().GetMethod(listenerFuncName, flags)?.Invoke(listenerObject, args);
+        __InvokeListener(listenerObject, listenerFuncName, flags, args);
+    }
+
+    private static void __InvokeGet(object current, BindingFlags flags, string func, PhpValue[] args)
+    {
+        var methodInfo = current.GetType().GetMethod(func, _bindingFlagsAll);
+        if (methodInfo != null) methodInfo.Invoke(current, CombineArgs(args));
+    }
+
+    private static void __InvokeListener(object listenerObject, string listenerFuncName, BindingFlags flags, PhpValue[] args)
+    {
+        listenerObject.GetType().GetMethod(listenerFuncName, flags)?.Invoke(listenerObject, CombineArgs(args));
+    }
+
+    private static object[] CombineArgs(PhpValue[] args)
+    {
+        object[] o= {};
+        if (args == null)
+        {
+            o = null;
+        }
+        else
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                o[i] = args[i];
+            }
+        }
+
+        return o;
     }
 }
