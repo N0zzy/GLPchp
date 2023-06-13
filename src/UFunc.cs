8088c441c99cf40152377244e9d74ef670668b5a
N0zzy <ffnn83@yandex.ru>
2023-06-13 16:40:22 +0500

    0.2.2

---
+++ src/UFunc.cs
@@ -0,0 +1,47 @@
+﻿using Godot;
+using Pchp.Core;
+
+namespace GLPchp;
+
+public sealed class UFunc: Service.Utils
+{
+    public static Callable Add(PhpCallable action, PhpArray args = null)
+    {
+        return Callable.From(() =>
+        {
+            action.Invoke(null, PhpValueArgs(args));
+        });
+    }
+    
+    public static Callable Bind(PhpValue current, string func, PhpArray args = null)
+    {
+        return Callable.From(() =>
+        {
+            CallableInvoke(current, func, _bindingFlagsBasic, args);
+        });
+    }
+    
+    public static Callable BindAll(PhpValue current, string func, PhpArray args = null)
+    {
+        return Callable.From(() =>
+        {
+            CallableInvoke(current, func, _bindingFlagsAll, args);
+        });
+    }
+    
+    public static Callable Listener(PhpArray listener, PhpArray args = null)
+    {
+        return Callable.From(() =>
+        {
+            CallableListener(listener, _bindingFlagsBasic, args);
+        });
+    }
+    
+    public static Callable ListenerAll(PhpArray listener, PhpArray args = null)
+    {
+        return Callable.From(() =>
+        {
+            CallableListener(listener, _bindingFlagsAll, args);
+        });
+    }
+}
---
+++ src/UxEvent.cs
@@ -0,0 +1,34 @@
+﻿using Godot;
+using Pchp.Core;
+
+namespace GLPchp;
+
+public sealed class UxEvent: Service.Utils
+{
+    public void On(string @event, PhpValue current, PhpCallable func, PhpArray args = null)
+    {
+        GodotObject gObj = (GodotObject)current.AsObject(); 
+        gObj.Connect(new StringName(@event), Callable.From(() =>
+        {
+            func.Invoke(null, args);
+        }));
+    }
+    
+    public void OnListener(string @event, PhpValue current, PhpArray listener, PhpArray args = null)
+    {
+        GodotObject gObj = (GodotObject)current.AsObject(); 
+        gObj.Connect(new StringName(@event), Callable.From(() =>
+        {
+            CallableListener(listener, _bindingFlagsBasic, args);
+        }));
+    }
+    
+    public void OnListenerAll(string @event, PhpValue current, PhpArray listener, PhpArray args = null)
+    {
+        GodotObject gObj = (GodotObject)current.AsObject(); 
+        gObj.Connect(new StringName(@event), Callable.From(() =>
+        {
+            CallableListener(listener, _bindingFlagsAll, args);
+        }));
+    }
+}
