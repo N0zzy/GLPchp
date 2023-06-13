a22abab98762c9ac98d145d436997a785596caa9
N0zzy <ffnn83@yandex.ru>
2023-06-13 16:56:51 +0500

    0.2.3

--- src/UxEvent.cs
+++ src/UxEvent.cs
@@ -7,28 +7,28 @@ public sealed class UxEvent: Service.Utils
 {
     public void On(string @event, PhpValue current, PhpCallable func, PhpArray args = null)
     {
-        GodotObject gObj = (GodotObject)current.AsObject(); 
-        gObj.Connect(new StringName(@event), Callable.From(() =>
-        {
+        Execute(@event, current, Callable.From(() => {
             func.Invoke(null, args);
         }));
     }
     
     public void OnListener(string @event, PhpValue current, PhpArray listener, PhpArray args = null)
     {
-        GodotObject gObj = (GodotObject)current.AsObject(); 
-        gObj.Connect(new StringName(@event), Callable.From(() =>
-        {
+        Execute(@event, current, Callable.From(() => {
             CallableListener(listener, _bindingFlagsBasic, args);
         }));
     }
     
     public void OnListenerAll(string @event, PhpValue current, PhpArray listener, PhpArray args = null)
     {
-        GodotObject gObj = (GodotObject)current.AsObject(); 
-        gObj.Connect(new StringName(@event), Callable.From(() =>
-        {
+        Execute(@event, current, Callable.From(() => {
             CallableListener(listener, _bindingFlagsAll, args);
         }));
+    }
+
+    private void Execute(string @event, PhpValue current, Callable callable)
+    {
+        GodotObject gObj = (GodotObject)current.AsObject(); 
+        gObj.Connect(new StringName(@event), callable);
     }
 }
